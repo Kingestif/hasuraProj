42dc4b42c24db653f28d9c76fa6272d54da1701c
Kingestif <kingestiff@gmail.com>
2025-10-07 02:07:35 +0300

    feat: finalize sign up an login

--- app.ts
+++ app.ts
@@ -1,11 +1,13 @@
+dotenv.config()
 import express from "express"
 import bcrypt from "bcrypt"
 import dotenv from "dotenv"
 import jwt from "jsonwebtoken"
+import { PrismaClient } from "./generated/prisma"
+const prisma = new PrismaClient()
 
 
 const app = express()
-dotenv.config()
 const port = process.env.PORT
 
 app.use(express.json())
@@ -14,9 +16,9 @@ app.listen(port, ()=>{
     console.log(`Server Started running at port ${port}`)
 })
 
-app.post('/signup', (req, res)=>{
+app.post('/signup', async(req, res)=>{
     try{
-        if(!req.body.email || !req.body.password){
+        if(!req.body.email || !req.body.password || !req.body.role){
             throw new Error("Empty Request Parameters")
         }
         
@@ -25,17 +27,19 @@ app.post('/signup', (req, res)=>{
 
         if(!saltRound) throw new Error("Cant' find salt round")
 
-        bcrypt.hash(password, saltRound, (err, hash)=>{
-            if(err){
-                return res.status(500).json({
-                    message: "cant hash the password"
-                })
+        const hash = await bcrypt.hash(password, saltRound);
+
+        const user = await prisma.user.create({
+            data: {
+                email: req.body.email,
+                password: hash,
+                role: req.body.role
             }
-                
-            return res.status(200).json({
-                message: "register success",
-                password:hash
-            })
+        })
+
+        return res.status(200).json({
+            message:"user registered succesfully",
+            user
         })
     
     }catch(err){
@@ -46,35 +50,43 @@ app.post('/signup', (req, res)=>{
     }
 })
 
-app.post('/login', (req, res)=>{
+app.post('/login', async(req, res)=>{
     try{
+        const users = await prisma.user.findMany();
         if(!req.body.email || !req.body.password){
             throw new Error("Request parameters can't be empty")
         }
         const password = req.body.password;
-        const hashedPass = ""
+        const user = await prisma.user.findUnique({
+            where:{email:req.body.email}
+        })
 
-        bcrypt.compare(password, hashedPass,  (err, result)=>{
-            if(err){
-                console.log(hashedPass)
-                return res.status(401).json({
-                    message: "Wrong email or password"
-                })
-            }
-
-            if(!process.env.JWTSECRET || typeof(process.env.JWTSECRET) !== "string"){
-                return res.status(500).json({
-                    message: "can't read jwt secret"
-                })
-            }
-            const jwtSecret = process.env.JWTSECRET;
-
-            const token = jwt.sign({email: req.body.email}, jwtSecret)
-
-            return res.status(200).json({
-                message: "Authorized",
-                accessToken: token
+        if(!user){
+            return res.status(404).json({
+                message: "user not found"
             })
+        }
+        const hashedPass = user.password;
+
+        const result = await bcrypt.compare(password, hashedPass)
+        if(!result){
+            return res.status(401).json({
+                message: "incorrect email or password"
+            })
+        }
+
+        if(!process.env.JWTSECRET || typeof(process.env.JWTSECRET) !== "string"){
+            return res.status(500).json({
+                message: "can't read jwt secret"
+            })
+        }
+        const jwtSecret = process.env.JWTSECRET;
+
+        const token = jwt.sign({email: req.body.email}, jwtSecret)
+
+        return res.status(200).json({
+            message: "Authorized",
+            accessToken: token
         })
 
     }catch(err){
